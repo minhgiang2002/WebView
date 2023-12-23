d43c670ade016766937ca8c5d9072ad0a5ef52f1
PC <minhgk2002@gmail.com>
2023-12-23 12:45:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.net.Uri;
+import android.content.Intent;
+import android.webkit.WebView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Intent intent = getIntent();
+        Uri data = intent.getData();
+
+        if (data != null) {
+            // Handle the deep link (data.toString())
+            WebView webView = findViewById(R.id.webview);
+            webView.loadUrl(data.toString());
+        }
+    }
+}
